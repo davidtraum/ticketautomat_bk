eefcac86ae69d41c16427488dc9764e9f877e093
davidtraum <david@traum.me>
2021-05-06 08:12:04 +0200

    Add ticket.cs

--- Ticketautomat/Form1.cs
+++ Ticketautomat/Form1.cs
@@ -51,5 +51,10 @@ namespace Ticketautomat
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Ticketautomat/Ticket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketautomat_Test
+{
+    class Ticket
+    {
+        private string description;
+        private double price;
+        private int ticketNumber;
+        private double paid = 0;
+
+        public Ticket(int pTicketNumber, double pPrice, string pDescription)
+        {
+            this.ticketNumber = pTicketNumber;
+            this.price = pPrice;
+            this.description = pDescription;
+        }
+
+        public string getDescription()
+        {
+            return this.description;
+        }
+
+        public void setDescription(string pDescription)
+        {
+            this.description = pDescription;
+        }
+
+        public double getPrice()
+        {
+            return this.price;
+        }
+
+        public void setPrice(float price)
+        {
+            this.price = price;
+        }
+
+        public int getTicketNumber()
+        {
+            return this.ticketNumber;
+        }
+
+        public bool pay(double amount)
+        {
+            this.paid += amount;
+            return this.getPaid() >= this.getPrice();
+        }
+
+        public double getExchange()
+        {
+            return this.getPaid() >= this.getPrice() ? this.getPaid() - this.getPrice() : 0;
+        }
+
+        public double getPaid()
+        {
+            return this.paid;
+        }
+    }
+}
